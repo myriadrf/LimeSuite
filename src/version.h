0eb9cb45e8d624c9352f3044f43bd4932ad1d19c
rjonaitis <richard.jonaitis@gmail.com>
2015-07-15 16:39:53 +0300

    merged in develop changes

--- src/version.h
+++ src/version.h
@@ -9,8 +9,8 @@ namespace AutoVersion
     static const int month = 7;
     static const int day = 15;
     static const int hour = 16;
-    static const int minutes = 20;
-    static const int seconds = 15;
-    static const int buildsCounter = 415;
-    static const char branchName[] = "develop";
+    static const int minutes = 38;
+    static const int seconds = 25;
+    static const int buildsCounter = 417;
+    static const char branchName[] = "";
 }
