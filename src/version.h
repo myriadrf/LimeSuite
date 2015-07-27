2556a3beb1470018e300fd855bec0d9b768484f2
Ricardas Jonaitis <richard.jonaitis@gmail.com>
2015-07-27 15:24:54 +0300

    renamed RF-Spark to RF-ESpark, RF-ESpark module now updates data when opening module

--- src/version.h
+++ src/version.h
@@ -7,10 +7,10 @@ namespace AutoVersion
 {
     static const int year = 2015;
     static const int month = 7;
-    static const int day = 22;
-    static const int hour = 11;
-    static const int minutes = 55;
-    static const int seconds = 54;
-    static const int buildsCounter = 462;
+    static const int day = 27;
+    static const int hour = 15;
+    static const int minutes = 22;
+    static const int seconds = 4;
+    static const int buildsCounter = 467;
     static const char branchName[] = "develop";
 }
