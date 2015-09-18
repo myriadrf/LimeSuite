5f929ba038ece8941e765e1fbcb288838633c633
Ricardas Jonaitis <richard.jonaitis@gmail.com>
2015-09-18 17:01:45 +0300

    fixed com port names on windows when not using unicode

--- src/version.h
+++ src/version.h
@@ -8,9 +8,9 @@ namespace AutoVersion
     static const int year = 2015;
     static const int month = 9;
     static const int day = 18;
-    static const int hour = 14;
-    static const int minutes = 14;
-    static const int seconds = 17;
-    static const int buildsCounter = 516;
+    static const int hour = 16;
+    static const int minutes = 10;
+    static const int seconds = 55;
+    static const int buildsCounter = 518;
     static const char branchName[] = "develop";
 }
