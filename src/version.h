490b856fc0ab90426a0034d2eb8f141c9eee3823
Ricardas Jonaitis <richard.jonaitis@gmail.com>
2015-09-29 09:52:38 +0300

    added file not found status enumeration

--- src/version.h
+++ src/version.h
@@ -7,10 +7,10 @@ namespace AutoVersion
 {
     static const int year = 2015;
     static const int month = 9;
-    static const int day = 28;
-    static const int hour = 17;
-    static const int minutes = 18;
+    static const int day = 29;
+    static const int hour = 9;
+    static const int minutes = 51;
     static const int seconds = 38;
-    static const int buildsCounter = 527;
+    static const int buildsCounter = 534;
     static const char branchName[] = "develop";
 }
