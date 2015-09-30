2591710c097ce41e518090af105721ee6b5ac92f
Ricardas Jonaitis <richard.jonaitis@gmail.com>
2015-09-30 14:56:26 +0300

    fixed TxTSP TSGFCW gui control

--- src/version.h
+++ src/version.h
@@ -8,9 +8,9 @@ namespace AutoVersion
     static const int year = 2015;
     static const int month = 9;
     static const int day = 29;
-    static const int hour = 9;
-    static const int minutes = 51;
-    static const int seconds = 38;
-    static const int buildsCounter = 534;
+    static const int hour = 12;
+    static const int minutes = 14;
+    static const int seconds = 58;
+    static const int buildsCounter = 537;
     static const char branchName[] = "develop";
 }
