e59684f4a11a3716ff2f42c3af328c1ccbfaece5
Ricardas Jonaitis <richard.jonaitis@gmail.com>
2015-11-05 13:32:10 +0200

    updated tooltips for BIST tab

--- src/version.h
+++ src/version.h
@@ -9,8 +9,8 @@ namespace AutoVersion
     static const int month = 11;
     static const int day = 5;
     static const int hour = 13;
-    static const int minutes = 16;
-    static const int seconds = 32;
-    static const int buildsCounter = 604;
+    static const int minutes = 31;
+    static const int seconds = 1;
+    static const int buildsCounter = 606;
     static const char branchName[] = "develop";
 }
