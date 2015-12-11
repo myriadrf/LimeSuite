ed8d29e569d3f66fe2f5a9897b3c13a7ffdbae5b
Ricardas Jonaitis <richard.jonaitis@gmail.com>
2015-12-11 12:36:02 +0200

    added percentage sign for programming progress, added temperature units

--- src/lms7002m/lms7002_defines.h
+++ src/lms7002m/lms7002_defines.h
@@ -203,10 +203,11 @@ enum eADC_UNITS
 	CURRENT,
 	RESISTANCE,
 	POWER,
+	TEMPERATURE,
 	ADC_UNITS_COUNT
 };
 
-static const char  adc_units_text[][8] = {"", "V", "A", "Ohm", "W"};
+static const char  adc_units_text[][8] = {"", "V", "A", "Ohm", "W", "C"};
 
 static const char* adcUnits2string(const unsigned units)
 {
