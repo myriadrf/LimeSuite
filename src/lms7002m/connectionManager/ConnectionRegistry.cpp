0a0621773d61f16bf3e5f8d552caa294a5bc1463
Josh Blum <josh@joshknows.com>
2015-12-17 19:40:01 -0800

    copied over ConnectionRegistry work

---
+++ src/lms7002m/connectionManager/ConnectionRegistry.cpp
@@ -0,0 +1,149 @@
+/**
+    @file   ConnectionRegistry.cpp
+    @author Lime Microsystems (www.limemicro.com)
+    @brief  Implementation of connection registry
+*/
+
+#include "ConnectionRegistry.h"
+#include "IConnection.h"
+#include <mutex>
+#include <map>
+#include <stdexcept>
+#include <memory>
+
+/*******************************************************************
+ * Connection handle implementation
+ ******************************************************************/
+ConnectionHandle::ConnectionHandle(void):
+    index(-1)
+{
+    return;
+}
+
+std::string ConnectionHandle::serialize(void) const
+{
+    std::string out;
+
+    if (not type.empty()) out += ", type="+type;
+    if (not name.empty()) out += ", name="+name;
+    if (not addr.empty()) out += ", addr="+addr;
+    if (not serial.empty()) out += ", serial="+serial;
+    if (index != -1) out += ", index="+std::to_string(index);
+
+    if (not out.empty()) out = out.substr(2); //remove comma+space
+
+    return out;
+}
+
+/*******************************************************************
+ * Registry data structures
+ ******************************************************************/
+static std::mutex &registryMutex(void)
+{
+    static std::mutex mutex;
+    return mutex;
+}
+
+static std::map<std::string, ConnectionRegistryEntry *> registryEntries;
+
+struct SharedConnection
+{
+    SharedConnection(void):
+        numInstances(0),
+        connection(nullptr)
+    {
+        return;
+    }
+    size_t numInstances;
+    IConnection *connection;
+};
+
+static std::map<std::string, std::shared_ptr<SharedConnection>> connectionCache;
+
+/*******************************************************************
+ * Registry implementation
+ ******************************************************************/
+std::vector<ConnectionHandle> ConnectionRegistry::findConnections(const ConnectionHandle &hint)
+{
+    std::lock_guard<std::mutex> lock(registryMutex());
+
+    std::vector<ConnectionHandle> result;
+    for (const auto &entry : registryEntries)
+    {
+        const auto r = entry.second->enumerate(hint);
+        result.insert(result.end(), r.begin(), r.end());
+    }
+    return result;
+}
+
+IConnection *ConnectionRegistry::makeConnection(const ConnectionHandle &handle)
+{
+    std::lock_guard<std::mutex> lock(registryMutex());
+
+    //use the identifier as a hint to perform a discovery
+    //only identifiers from the discovery function itself is used in the factory
+    for (const auto &entry : registryEntries)
+    {
+        const auto r = entry.second->enumerate(handle);
+        if (r.empty()) continue;
+
+        const auto realHandle = r.front(); //just pick the first
+
+        //check the cache
+        auto &sharedConnection = connectionCache[realHandle.serialize()];
+        if (not sharedConnection)
+        {
+            //cache entry is empty, make a new connection
+            sharedConnection.reset(new SharedConnection());
+            try
+            {
+                sharedConnection->connection = entry.second->make(realHandle);
+            }
+            catch (...)
+            {
+                //factory failed, erase entry and re-throw
+                connectionCache.erase(realHandle.serialize());
+                throw;
+            }
+        }
+
+        //return from cache, increment ref count
+        sharedConnection->numInstances++;
+        return sharedConnection->connection;
+    }
+
+    throw std::runtime_error("No connections found for " + handle.serialize());
+}
+
+void ConnectionRegistry::freeConnection(IConnection *conn)
+{
+    std::lock_guard<std::mutex> lock(registryMutex());
+
+    for (auto &cacheEntry : connectionCache)
+    {
+        if (not cacheEntry.second) continue;
+        if (cacheEntry.second->connection != conn) continue;
+        cacheEntry.second->numInstances--;
+        if (cacheEntry.second->numInstances != 0) continue;
+        delete cacheEntry.second->connection;
+        cacheEntry.second.reset();
+    }
+
+    //we never actually clear out the cache entries when they are empty
+    //we can do it here, but they should never really grow indefinitely
+}
+
+/*******************************************************************
+ * Entry implementation
+ ******************************************************************/
+ConnectionRegistryEntry::ConnectionRegistryEntry(const std::string &name)
+{
+    std::lock_guard<std::mutex> lock(registryMutex());
+    registryEntries[_name] = this;
+}
+
+ConnectionRegistryEntry::~ConnectionRegistryEntry(void)
+{
+    std::lock_guard<std::mutex> lock(registryMutex());
+    registryEntries.erase(_name);
+}
---
+++ src/lms7002m/connectionManager/ConnectionRegistry.h
@@ -0,0 +1,141 @@
+/**
+    @file ConnectionRegistry.h
+    @author Lime Microsystems
+    @brief Registration and access for possible connection types.
+*/
+
+#ifndef CONNECTION_REGISTRY_H
+#define CONNECTION_REGISTRY_H
+
+class IConnection;
+
+#include <string>
+#include <vector>
+
+/*!
+ * A connection handle identifies a particular connection.
+ * Handles are used by the connection registry to return
+ * enumeration results and to instantiate connections.
+ *
+ * When using ConnectionHandle to specify an enumeration hint,
+ * simply fill in only the fields that the user is interested in.
+ * Each default field (empty string, -1 index) will be ignored
+ * by the connection registry entry discovery implementation.
+ */
+class ConnectionHandle
+{
+public:
+
+    ConnectionHandle(void);
+
+    /*!
+     * The connection type. Example: SPI, COM, USB.
+     */
+    std::string type;
+
+    /*!
+     * The name of the device. Example: STREAM, NOVENA.
+     */
+    std::string name;
+
+    /*!
+     * An address such a URL, IP address, or similar.
+     */
+    std::string addr;
+
+    /*!
+     * A serial number which is unique to a particular device.
+     */
+    std::string serial;
+
+    /*!
+     * An internal index which may enumerate device handles.
+     */
+    long long index;
+
+    /*!
+     * Serialize this connection handle into a string format.
+     * This string format can be used to represent the handle.
+     */
+    std::string serialize(void) const;
+};
+
+/*!
+ * The connection registry provides a way to register
+ * discovery methods and factories for known connections,
+ * and to query and instantiate available connections.
+ */
+class ConnectionRegistry
+{
+public:
+
+    /*!
+     * Discovery identifiers that can be used to create a connection.
+     * The hint may contain a connection type, serial number, ip address, etc.
+     * \param hint an optional connection handle with some fields filled-in
+     * \return a list of handles which can be used to make a connection
+     */
+    std::vector<ConnectionHandle> findConnections(const ConnectionHandle &hint = ConnectionHandle());
+
+    /*!
+     * Create a connection from an identifying handle.
+     * \param handle a connection handle with fields filled-in
+     * \return a pointer to a connection instance
+     */
+    IConnection *makeConnection(const ConnectionHandle &handle);
+
+    /*!
+     * Free an connection created by makeConnection().
+     */
+    void freeConnection(IConnection *conn);
+};
+
+/*******************************************************************
+ * This section below is the registry API for connections.
+ * The registry API is intended for device developers.
+ ******************************************************************/
+
+/*!
+ * Create an overloaded instance of a ConnectionRegistryEntry
+ * to register discovery and factory functions into the system.
+ * The ConnectionRegistryEntry should be created prior to
+ * discovering and instantiating connections.
+ * One recommended use is at static initialization time.
+ */
+class ConnectionRegistryEntry
+{
+public:
+
+    /*!
+     * Instantiating the ConnectionRegistryEntry
+     * registers it into the connection registry.
+     * The name describes the type of connection,
+     * that should be unique to the entry instance.
+     */
+    ConnectionRegistryEntry(const std::string &name);
+
+    //! Unregister a connection type
+    virtual ~ConnectionRegistryEntry(void);
+
+    /*!
+     * A discovery function takes a connection handle hint
+     * and returns a list of identifiers that can be used
+     * to create connection with makeConnection().
+     * \param hint an optional connection handle with some fields filled-in
+     * \return a list of handles which can be used to make a connection
+     */
+    virtual std::vector<ConnectionHandle> enumerate(const ConnectionHandle &hint) = 0;
+
+    /*!
+     * A factory function creates a connection from a connection handle.
+     * \param handle a connection handle with fields filled-in
+     * \return a pointer to a connection instance
+     */
+    virtual IConnection *make(const ConnectionHandle &handle) = 0;
+
+private:
+    std::string _name;
+};
+
+#endif
+
