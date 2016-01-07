ec15de734060e8856459a9d1f8ed82f8fed34b11
Ricardas Jonaitis <richard.jonaitis@gmail.com>
2016-01-07 13:33:22 +0200

    changed FIR coefficients used in calibration

--- src/version.h
+++ src/version.h
@@ -7,10 +7,10 @@ namespace AutoVersion
 {
     static const int year = 2016;
     static const int month = 1;
-    static const int day = 5;
-    static const int hour = 15;
-    static const int minutes = 2;
-    static const int seconds = 34;
-    static const int buildsCounter = 841;
+    static const int day = 7;
+    static const int hour = 13;
+    static const int minutes = 32;
+    static const int seconds = 10;
+    static const int buildsCounter = 843;
     static const char branchName[] = "develop";
 }
