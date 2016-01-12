d764f2130580da44f1446e5fbc28011788c78be0
Josh Blum <josh@joshknows.com>
2016-01-11 19:29:30 -0800

    reimplement around LTE streaming api

--- src/ConnectionSTREAM/ConnectionSTREAMing.cpp
+++ src/ConnectionSTREAM/ConnectionSTREAMing.cpp
@@ -6,6 +6,8 @@
 
 #include "ConnectionSTREAM.h"
 #include "LMS_StreamBoard.h"
+#include "StreamerLTE.h"
+#include "fifo.h" //from StreamerLTE
 #include <iostream>
 #include <thread>
 #include <chrono>
@@ -14,20 +16,144 @@
 
 #define STREAM_MTU 16384 //for now
 
+/***********************************************************************
+ * Custom StreamerLTE for streaming API hooks
+ **********************************************************************/
+
+struct StreamerLTECustom : StreamerLTE
+{
+    StreamerLTECustom(LMS64CProtocol *dataPort, const bool isTx, const size_t channelsCount, const StreamDataFormat format):
+        StreamerLTE(dataPort),
+        isTx(isTx),
+        channelsCount(channelsCount),
+        format(format),
+        workThread(nullptr),
+        mFIFO(nullptr)
+    {
+        std::cout << "StreamerLTECustom(isTx=" << isTx << ", channelsCount=" << channelsCount << ")" << std::endl;
+        if (workThread != nullptr) return;
+
+        mFIFO = new LMS_SamplesFIFO(2*4096, channelsCount);
+        for (size_t i = 0; i < channelsCount; i++)
+        {
+            mFIFOBuffers.push_back(new complex16_t[STREAM_MTU]);
+        }
+
+        //switch off Rx
+        uint16_t regVal = Reg_read(dataPort, 0x0005);
+        Reg_write(dataPort, 0x0005, regVal & ~0x6);
+
+        //enable MIMO mode, 12 bit compressed values
+        if (channelsCount == 2)
+        {
+            Reg_write(dataPort, 0x0001, 0x0003);
+            Reg_write(dataPort, 0x0007, 0x000A);
+        }
+        else
+        {
+            Reg_write(dataPort, 0x0001, 0x0001);
+            Reg_write(dataPort, 0x0007, 0x0008);
+        }
+
+        //USB FIFO reset
+        //TODO direction specific reset using isTx
+        LMS64CProtocol::GenericPacket ctrPkt;
+        ctrPkt.cmd = CMD_USB_FIFO_RST;
+        ctrPkt.outBuffer.push_back(0x01);
+        dataPort->TransferPacket(ctrPkt);
+        ctrPkt.outBuffer[0] = 0x00;
+        dataPort->TransferPacket(ctrPkt);
+
+        stopThread = false;
+        if (format == STREAM_12_BIT_COMPRESSED)
+        {
+            if (!isTx) workThread = new std::thread(ReceivePackets, dataPort, mFIFO, &stopThread, &rate_Bps);
+            if (isTx) workThread = new std::thread(TransmitPackets, dataPort, mFIFO, &stopThread, &rate_Bps);
+        }
+        else
+        {
+            if (!isTx) workThread = new std::thread(ReceivePacketsUncompressed, dataPort, mFIFO, &stopThread, &rate_Bps);
+            if (isTx) workThread = new std::thread(TransmitPacketsUncompressed, dataPort, mFIFO, &stopThread, &rate_Bps);
+        }
+    }
+
+    ~StreamerLTECustom(void)
+    {
+        std::cout << "~StreamerLTECustom " << std::endl;
+        if (workThread == nullptr) return;
+        stopThread = true;
+
+        workThread->join();
+        delete workThread;
+        workThread = nullptr;
+
+        delete mFIFO;
+        mFIFO = nullptr;
+
+        for (size_t i = 0; i < channelsCount; i++)
+        {
+            delete [] mFIFOBuffers[i];
+        }
+        mFIFOBuffers.clear();
+    }
+
+    void Enable(LMS64CProtocol *dataPort, const bool enable)
+    {
+        std::cout << "StreamerLTECustom::Enable " << enable << std::endl;
+        if (enable)
+        {
+            //switch on Rx
+            auto regVal = Reg_read(dataPort, 0x0005);
+            bool async = false;
+            Reg_write(dataPort, 0x0005, (regVal & ~0x20) | 0x6 | (async << 4));
+        }
+        else
+        {
+            //stop Tx Rx if they were active
+            auto regVal = Reg_read(dataPort, 0x0005);
+            Reg_write(dataPort, 0x0005, regVal & ~0x6);
+        }
+    }
+
+    const bool isTx;
+    const size_t channelsCount;
+    const StreamDataFormat format;
+    std::atomic<bool> stopThread;
+    std::atomic<uint32_t> rate_Bps;
+    std::thread *workThread;
+    LMS_SamplesFIFO *mFIFO;
+    std::vector<complex16_t *> mFIFOBuffers;
+};
+
+/***********************************************************************
+ * Streaming API implementation
+ **********************************************************************/
+
 std::string ConnectionSTREAM::SetupStream(size_t &streamID, const StreamConfig &config)
 {
     streamID = ~0;
 
-    if (config.isTx)
-        return "ConnectionSTREAM::setupStream() no TX yet";
-
+    //API format check
     if (config.format != StreamConfig::STREAM_COMPLEX_FLOAT32)
         return "ConnectionSTREAM::setupStream() only complex floats now";
 
-    if (config.channels.size() != 2 or config.channels.at(0) != 0 or config.channels.at(1) != 1)
-        return "SoapyIConnection::setupStream() only channels 0, 1 supported";
+    //check channel config
+    if (config.channels.size() > 2) return "SoapyIConnection::setupStream() 2 channels max";
+    if (config.channels.size() == 1 and config.channels.at(0) != 0) return "SoapyIConnection::setupStream() first channel must be 0";
+    if (config.channels.size() == 2 and config.channels.at(1) != 1) return "SoapyIConnection::setupStream() second channel must be 1";
+    size_t channelsCount = config.channels.size();
+    if (channelsCount == 0) channelsCount++;
 
-    streamID = size_t(new LMS_StreamBoard(this));
+    //check link format
+    auto linkFormat = StreamerLTE::STREAM_12_BIT_IN_16;
+    switch (config.linkFormat)
+    {
+    case StreamConfig::STREAM_12_BIT_IN_16: linkFormat = StreamerLTE::STREAM_12_BIT_IN_16; break;
+    case StreamConfig::STREAM_12_BIT_COMPRESSED: linkFormat = StreamerLTE::STREAM_12_BIT_COMPRESSED; break;
+    default: return "SoapyIConnection::setupStream() unsupported link format";
+    }
+
+    streamID = size_t(new StreamerLTECustom(this, config.isTx, channelsCount, linkFormat));
     return ""; //success
 }
 
@@ -44,42 +170,64 @@ size_t ConnectionSTREAM::GetStreamSize(const size_t streamID)
 
 bool ConnectionSTREAM::ControlStream(const size_t streamID, const bool enable, const size_t burstSize, const StreamMetadata &metadata)
 {
-    auto *stream = (LMS_StreamBoard *)streamID;
-    if (enable) stream->StartReceiving(STREAM_MTU);
-    else stream->StopReceiving();
+    auto *stream = (StreamerLTECustom *)streamID;
+    stream->Enable(this, enable);
 }
 
 int ConnectionSTREAM::ReadStream(const size_t streamID, void * const *buffs, const size_t length, const long timeout_ms, StreamMetadata &metadata)
 {
-    auto *stream = (LMS_StreamBoard *)streamID;
+    auto *stream = (StreamerLTECustom *)streamID;
 
-    const auto exitTime(std::chrono::high_resolution_clock::now() + std::chrono::milliseconds(timeout_ms));
-    do
+    size_t samplesCount = std::min<size_t>(length, STREAM_MTU);
+    size_t sampsPopped = stream->mFIFO->pop_samples(
+        stream->mFIFOBuffers.data(),
+        samplesCount,
+        stream->channelsCount,
+        &metadata.timestamp,
+        timeout_ms);
+    samplesCount = std::min(samplesCount, sampsPopped);
+
+    for (size_t i = 0; i < stream->channelsCount; i++)
     {
-        auto data = stream->GetIncomingData();
-        if (data.samplesI.size() != 0)
+        auto buffIn = stream->mFIFOBuffers[i];
+        auto buffOut = (std::complex<float> *)buffs[i];
+        for (size_t j = 0; j < samplesCount; j++)
         {
-            //TODO
-            // * both buffers
-            // * remainder when numElems is smaller
-            size_t N = std::min(data.samplesI.size(), length);
-            auto ch0_data = (std::complex<float> *)buffs[0];
-            for (size_t i = 0; i < N; i++)
-            {
-                ch0_data[i] = std::complex<float>(data.samplesI[i], data.samplesQ[i]);
-            }
-            return N;
+            buffOut[j] = std::complex<float>(
+                float(buffIn[j].i)/1024,
+                float(buffIn[j].q)/1024);
         }
-        std::this_thread::sleep_for(std::chrono::milliseconds(10));
+    }
 
-    } while (std::chrono::high_resolution_clock::now() < exitTime);
-
-    return 0;
+    return samplesCount;
 }
 
 int ConnectionSTREAM::WriteStream(const size_t streamID, const void * const *buffs, const size_t length, const long timeout_ms, const StreamMetadata &metadata)
 {
-    return -1;
+    auto *stream = (StreamerLTECustom *)streamID;
+
+    size_t samplesCount = std::min<size_t>(length, STREAM_MTU);
+
+    for (size_t i = 0; i < stream->channelsCount; i++)
+    {
+        auto buffIn = (const std::complex<float> *)buffs[i];
+        auto bufOut = stream->mFIFOBuffers[i];
+        for (size_t j = 0; j < samplesCount; j++)
+        {
+            bufOut[j].i = int16_t(buffIn[j].real()*1024);
+            bufOut[j].q = int16_t(buffIn[j].imag()*1024);
+        }
+    }
+
+    size_t sampsPushed = stream->mFIFO->push_samples(
+        (const complex16_t **)stream->mFIFOBuffers.data(),
+        samplesCount,
+        stream->channelsCount,
+        metadata.timestamp,
+        timeout_ms);
+    samplesCount = std::min(samplesCount, sampsPushed);
+
+    return samplesCount;
 }
 
 void ConnectionSTREAM::UpdateExternalDataRate(const size_t channel, const double txRate, const double rxRate)
