4948c9f8c3583434d57bbd965514235a708eccc6
Ricardas Jonaitis <richard.jonaitis@gmail.com>
2016-02-04 16:07:12 +0200

    update binary

--- src/version.h
+++ src/version.h
@@ -6,11 +6,11 @@
 namespace AutoVersion
 {
     static const int year = 2016;
-    static const int month = 1;
-    static const int day = 7;
-    static const int hour = 13;
-    static const int minutes = 32;
-    static const int seconds = 10;
-    static const int buildsCounter = 843;
+    static const int month = 2;
+    static const int day = 4;
+    static const int hour = 16;
+    static const int minutes = 6;
+    static const int seconds = 24;
+    static const int buildsCounter = 844;
     static const char branchName[] = "develop";
 }
