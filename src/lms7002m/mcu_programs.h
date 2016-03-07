17d44044b5cff7a7a073f2c6c3e10f39c57a3ecb
Ricardas Jonaitis <richard.jonaitis@gmail.com>
2016-03-07 14:03:24 +0200

    update MCU programs, add static arrays of MCU programs

---
+++ src/lms7002m/mcu_programs.h
@@ -0,0 +1,9 @@
+#ifndef LMS7_MCU_PROGRAMS_H
+#define LMS7_MCU_PROGRAMS_H
+
+#define MCU_PROGRAM_SIZE 8192
+const unsigned char mcu_program_lms7_dc_iq_calibration_hex[MCU_PROGRAM_SIZE];
+const unsigned char mcu_program_lms7_dc_iq_calibration_RxTDD_hex[MCU_PROGRAM_SIZE];
+
+#endif
+
