d84871202b488aaebeb9a576351d2ab18d1353f4
Josh Blum <josh@joshknows.com>
2016-04-08 16:48:47 -0700

    plotting and corrections tweaks

--- src/SoapyLMS7/LimeSuiteCalibrate.py
+++ src/SoapyLMS7/LimeSuiteCalibrate.py
@@ -27,7 +27,7 @@ PAD_GAIN = -10.0
 LB_PAD_GAIN = 0.0
 
 TX_FREQ_DELTA = 0.7e6
-TX_CORDIC_FREQ = 1.0e6
+TX_CORDIC_FREQ = [1.1e6, 1.5e6]
 
 MAX_SEARCH_DEPTHS = range(2, 11)
 NUM_STEPS_PER_ITER = 3
@@ -60,7 +60,7 @@ def readSamps(limeSDR, rxStream, numSamps=SAMPS_PER_CAPTURE):
 def measureToneLevel(samps, freq, rate=SAMPLE_RATE):
     step = -2.0*math.pi*(freq)/rate
     shifted = samps * np.exp(1j*np.linspace(0, samps.size*step, samps.size))
-    return abs(np.mean(shifted))
+    return 20*math.log10(max(1e-20, abs(np.mean(shifted))))
 
 ##########################################
 ## Plotting utilities
@@ -81,7 +81,6 @@ def sampsToPowerFFT(rxSamples, fftSize=NUM_BINS_PER_FFT):
         windowPower = sum(window**2)/fftSize
         absBins = np.abs(np.fft.fft(x*window))
         absBins = np.concatenate((absBins[absBins.size/2:], absBins[:absBins.size/2])) #reorder
-        if (absBins.size % 2) == 0: absBins = absBins[1:]
         absBins = np.maximum(absBins, 1e-20) #clip
         return 20*np.log10(absBins) - 20*math.log10(absBins.size) - 10*math.log10(windowPower)
 
@@ -92,35 +91,42 @@ def sampsToPowerFFT(rxSamples, fftSize=NUM_BINS_PER_FFT):
 
     return np.log(sum(fftSet)/len(fftSet))
 
-def plotSingleResult(rxInitial, rxFinal, txInitial, txFinal, rate=SAMPLE_RATE):
+def plotSingleResult(rxInitial, rxFinal, txInitial, txFinal, freq, rate=SAMPLE_RATE):
     import matplotlib.pyplot as plt
-    fig = plt.figure(figsize=(20, 10), dpi=80)
+    fig = plt.figure(figsize=(16, 9))
+    fig.suptitle('Digital corrections @ %g MHz'%(freq/1e6), fontsize=12)
 
-    for samps, idx, title, initial, isTx in (
-        (rxInitial[0], 1, 'Rx ChA Initial', True, False),
-        (rxInitial[1], 2, 'Rx ChB Initial', True, False),
-        (rxFinal[0], 3, 'Rx ChA Corrected', False, False),
-        (rxFinal[1], 4, 'Rx ChB Corrected', False, False),
-        (txInitial[0], 5, 'Tx ChA Initial', True, True),
-        (txInitial[1], 6, 'Tx ChB Initial', True, True),
-        (txFinal[0], 7, 'Tx ChA Corrected', False, True),
-        (txFinal[1], 8, 'Tx ChB Corrected', False, True),
+    for samps, idx, initial, isTx, ch in (
+        (rxInitial, 1, True, False, 0),
+        (rxInitial, 2, True, False, 1),
+        (rxFinal, 3, False, False, 0),
+        (rxFinal, 4, False, False, 1),
+        (txInitial, 5, True, True, 0),
+        (txInitial, 6, True, True, 1),
+        (txFinal, 7, False, True, 0),
+        (txFinal, 8, False, True, 1),
     ):
+        otherCh = (ch + 1)%2
+        chName = {0:'ChA', 1:'ChB'}[ch]
+        otherChName = {0:'ChA', 1:'ChB'}[otherCh]
+        title = "%s %s %s"%('Tx' if isTx else 'Rx', chName, 'Initial' if initial else 'Corrected')
 
-        fftBins = sampsToPowerFFT(samps)
+        fftBins = sampsToPowerFFT(samps[ch])
         ax = fig.add_subplot(4, 2, idx)
-        ax.plot(np.arange(-rate/2/1e6, rate/2/1e6, rate/fftBins.size/1e6)[:fftBins.size], fftBins)
+        ax.plot(np.arange(-rate/2/1e6, rate/2/1e6, rate/fftBins.size/1e6)[:fftBins.size], fftBins, label=title)
         ax.grid(True)
-        ax.set_title(title, fontsize=8)
+        #ax.set_title(title, fontsize=8)
         #ax.set_xlabel('Freq (MHz)', fontsize=8)
         ax.set_ylabel('Power (dBfs)', fontsize=8)
         ax.set_ylim(top=-10, bottom=-90)
+        ax.locator_params(axis='y', nbins=6)
+        legend = ax.legend(loc='upper left', fontsize=10)
 
         def annotate(label, freq, offset, color):
             x = int(fftBins.size*freq/rate) - fftBins.size/2
             power = max(fftBins[x-3:x+3])
 
-            if initial: ax.annotate(label,
+            if initial: ax.annotate(label, fontsize=8,
                 xy = (freq/1e6, power), xytext = (freq/1e6+offset[0], power+offset[1]),
                 xycoords = 'data', textcoords = 'data',
                 arrowprops = dict(arrowstyle = '->'))
@@ -128,9 +134,10 @@ def plotSingleResult(rxInitial, rxFinal, txInitial, txFinal, rate=SAMPLE_RATE):
             ax.scatter(freq/1e6, power, c=color, s=50)
 
         if isTx:
-            annotate("Tx tone", TX_FREQ_DELTA + TX_CORDIC_FREQ, (0.5, -10), 'green')
+            annotate("Tx tone", TX_FREQ_DELTA + TX_CORDIC_FREQ[ch], (0.5, -10), 'green')
             annotate("Tx dc", TX_FREQ_DELTA, (-1, 10), 'yellow')
-            annotate("Tx imbal", TX_FREQ_DELTA - TX_CORDIC_FREQ, (-1, 10), 'red')
+            annotate("Tx imbal", TX_FREQ_DELTA - TX_CORDIC_FREQ[ch], (-1, 10), 'red')
+            annotate("%s\nleak"%otherChName, TX_FREQ_DELTA + TX_CORDIC_FREQ[otherCh], (0.5-ch, 10), 'blue')
 
         else:
             annotate("Tx tone", TX_FREQ_DELTA, (0.5, -10), 'green')
@@ -146,7 +153,7 @@ def GenerateTestPoints(depth):
 
     scale = 1.0/(2**depth)
     phaseMax = (math.pi/2)*scale
-    gainMax = 0.5*scale
+    gainMax = 1.0*scale
     offMax = 1.0*scale
 
     dcPoints = list()
@@ -157,7 +164,7 @@ def GenerateTestPoints(depth):
     iqPoints = list()
     for phase in np.linspace(-phaseMax, phaseMax, NUM_STEPS_PER_ITER):
         for gain in np.linspace(-gainMax, gainMax, NUM_STEPS_PER_ITER):
-            iqPoints.append(cmath.rect(1.0 + gain, phase))
+            iqPoints.append(cmath.rect(2**gain, phase))
 
     return zip(dcPoints, iqPoints)
 
@@ -182,9 +189,9 @@ def CalibrateAtFreq(limeSDR, rxStream, freq):
     for ch in [0, 1]: limeSDR.setGain(SOAPY_SDR_RX, ch, "PGA", PGA_GAIN)
     samps = readSamps(limeSDR, rxStream)
     for ch in [0, 1]:
-        lvl = measureToneLevel(samps[ch], TX_FREQ_DELTA)
-        deltadB = -10 - 20*math.log10(lvl)
-        print 'deltadB', deltadB, lvl, 20*math.log10(lvl)
+        lvldB = measureToneLevel(samps[ch], TX_FREQ_DELTA)
+        deltadB = -10 - lvldB
+        print('deltadB=%f, lvldB=%f'%(deltadB, lvldB))
         limeSDR.setGain(SOAPY_SDR_RX, ch, "PGA", min(19, PGA_GAIN + deltadB))
 
     #sweep for best Rx IQ correction
@@ -197,7 +204,7 @@ def CalibrateAtFreq(limeSDR, rxStream, freq):
             newIqCorrs = list()
             for ch in [0, 1]:
                 newIqCorr = cmath.rect(
-                    abs(iqPoint) + abs(bestRxIqCorrsIter[ch]) - 1.0,
+                    abs(iqPoint) * abs(bestRxIqCorrsIter[ch]),
                     cmath.phase(iqPoint) + cmath.phase(bestRxIqCorrsIter[ch]))
                 limeSDR.setIQBalance(SOAPY_SDR_RX, ch, newIqCorr)
                 newIqCorrs.append(newIqCorr)
@@ -213,7 +220,7 @@ def CalibrateAtFreq(limeSDR, rxStream, freq):
     rxFinal = readSamps(limeSDR, rxStream)
 
     #sweep for best Tx IQ  and DC correction
-    for ch in [0, 1]: limeSDR.setFrequency(SOAPY_SDR_TX, ch, "BB", TX_CORDIC_FREQ)
+    for ch in [0, 1]: limeSDR.setFrequency(SOAPY_SDR_TX, ch, "BB", TX_CORDIC_FREQ[ch])
     txInitial = readSamps(limeSDR, rxStream)
     bestTxIqCorrs = [1.0]*2
     bestTxIqLevels = [1.0]*2
@@ -227,7 +234,7 @@ def CalibrateAtFreq(limeSDR, rxStream, freq):
             newDcCorrs = list()
             for ch in [0, 1]:
                 newIqCorr = cmath.rect(
-                    abs(iqPoint) + abs(bestTxIqCorrsIter[ch]) - 1.0,
+                    abs(iqPoint) * abs(bestTxIqCorrsIter[ch]),
                     cmath.phase(iqPoint) + cmath.phase(bestTxIqCorrsIter[ch]))
                 newDcCorr = complex(
                     max(min(bestTxDcCorrsIter[ch].real + dcPoint.real, 1.0), -1.0),
@@ -238,7 +245,7 @@ def CalibrateAtFreq(limeSDR, rxStream, freq):
                 newDcCorrs.append(newDcCorr)
             samps = readSamps(limeSDR, rxStream)
             for ch in [0, 1]:
-                lvl = measureToneLevel(samps[ch], TX_FREQ_DELTA-TX_CORDIC_FREQ)
+                lvl = measureToneLevel(samps[ch], TX_FREQ_DELTA-TX_CORDIC_FREQ[ch])
                 if lvl < bestTxIqLevels[ch]:
                     bestTxIqCorrs[ch] = newIqCorrs[ch]
                     bestTxIqLevels[ch] = lvl
@@ -254,7 +261,10 @@ def CalibrateAtFreq(limeSDR, rxStream, freq):
         limeSDR.setDCOffset(SOAPY_SDR_TX, ch, bestTxDcCorrs[ch])
     txFinal = readSamps(limeSDR, rxStream)
 
-    plotSingleResult(rxInitial, rxFinal, txInitial, txFinal)
+    plotSingleResult(
+        rxInitial=rxInitial, rxFinal=rxFinal,
+        txInitial=txInitial, txFinal=txFinal,
+        freq=freq)
 
 ##########################################
 ## Main calibration sweep
