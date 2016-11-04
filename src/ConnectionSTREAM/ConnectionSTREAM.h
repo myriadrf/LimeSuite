b83862519e6a0335d770afe20172306b9d204e3a
ignasj <ignas.jarusevicius@gmail.com>
2016-11-04 10:57:33 +0200

    Make default timeout parameter same for derived IConnection classes

--- src/ConnectionSTREAM/ConnectionSTREAM.h
+++ src/ConnectionSTREAM/ConnectionSTREAM.h
@@ -98,8 +98,8 @@ public:
     bool IsOpen();
     int GetOpenedIndex();
 
-    virtual int Write(const unsigned char* buffer, int length, int timeout_ms = 0) override;
-    virtual int Read(unsigned char* buffer, int length, int timeout_ms = 0) override;
+    virtual int Write(const unsigned char* buffer, int length, int timeout_ms = 100) override;
+    virtual int Read(unsigned char* buffer, int length, int timeout_ms = 100) override;
 
     virtual int UploadWFM(const void* const* samples, uint8_t chCount, size_t sample_count, StreamConfig::StreamDataFormat format) override;
 
