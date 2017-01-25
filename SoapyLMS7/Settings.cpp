1daa1ed7fedd5e8e9ae45266cf7fb99ea4af04d8
Josh Blum <josh@joshknows.com>
2017-01-25 01:27:21 -0800

    Fix NCO tuning sign in SoapyLMS7 for LMS7 silicon rev

--- SoapyLMS7/Settings.cpp
+++ SoapyLMS7/Settings.cpp
@@ -559,15 +559,17 @@ void SoapyLMS7::setFrequency(const int direction, const size_t channel, const st
 
     if (name == "BB")
     {
+        int pos = (rfic->Get_SPI_Reg_bits(LMS7_MASK, true) == 0)? 0 : 1;
+        int neg = pos ^ 0x1;
         switch (direction)
         {
         case SOAPY_SDR_RX:
             rfic->Modify_SPI_Reg_bits(LMS7param(CMIX_BYP_RXTSP), (frequency == 0)?1:0);
-            rfic->Modify_SPI_Reg_bits(LMS7param(CMIX_SC_RXTSP), (frequency < 0)?1:0);
+            rfic->Modify_SPI_Reg_bits(LMS7param(CMIX_SC_RXTSP), (frequency < 0)?neg:pos);
             break;
         case SOAPY_SDR_TX:
             rfic->Modify_SPI_Reg_bits(LMS7param(CMIX_BYP_TXTSP), (frequency == 0)?1:0);
-            rfic->Modify_SPI_Reg_bits(LMS7param(CMIX_SC_TXTSP), (frequency < 0)?1:0);
+            rfic->Modify_SPI_Reg_bits(LMS7param(CMIX_SC_TXTSP), (frequency < 0)?neg:pos);
             break;
         }
         if (rfic->SetNCOFrequency(lmsDir, 0, abs(frequency)) != 0)
@@ -595,7 +597,8 @@ double SoapyLMS7::getFrequency(const int direction, const size_t channel, const 
 
     if (name == "BB")
     {
-        int sign = rfic->Get_SPI_Reg_bits(lmsDir==LMS7002M::Tx?LMS7param(CMIX_SC_TXTSP):LMS7param(CMIX_SC_RXTSP)) == 0 ? 1 : -1;
+        int pos = (rfic->Get_SPI_Reg_bits(LMS7_MASK, true) == 0)? 0 : 1;
+        int sign = rfic->Get_SPI_Reg_bits(lmsDir==LMS7002M::Tx?LMS7param(CMIX_SC_TXTSP):LMS7param(CMIX_SC_RXTSP)) == pos ? 1 : -1;
         return rfic->GetNCOFrequency(lmsDir, 0) * sign;
     }
 
