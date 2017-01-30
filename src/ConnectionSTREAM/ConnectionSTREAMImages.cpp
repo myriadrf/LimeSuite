10e0cfb87c0c9d92a99ca4b34cf3bf9aa93673d9
Josh Blum <josh@joshknows.com>
2017-01-30 00:46:19 -0800

    Updated LimeSDR rev1.4 gateware images to version 2.6

--- src/ConnectionSTREAM/ConnectionSTREAMImages.cpp
+++ src/ConnectionSTREAM/ConnectionSTREAMImages.cpp
@@ -39,7 +39,7 @@ static const ConnectionSTREAMImageEntry &lookupImageEntry(const LMS64CProtocol::
 {
     static const std::vector<ConnectionSTREAMImageEntry> imageEntries = {
         ConnectionSTREAMImageEntry({LMS_DEV_UNKNOWN, -1, -1, "Unknown-USB.img", -1, -1, "Unknown-USB.rbf"}),
-        ConnectionSTREAMImageEntry({LMS_DEV_LIMESDR, 4, 3, "LimeSDR-USB_HW_1.3_r3.0.img", 2, 2,  "LimeSDR-USB_HW_1.4_r2.2.rbf"}),
+        ConnectionSTREAMImageEntry({LMS_DEV_LIMESDR, 4, 3, "LimeSDR-USB_HW_1.3_r3.0.img", 2, 6,  "LimeSDR-USB_HW_1.4_r2.6.rbf"}),
         ConnectionSTREAMImageEntry({LMS_DEV_LIMESDR, 3, 3, "LimeSDR-USB_HW_1.3_r3.0.img", 1, 20, "LimeSDR-USB_HW_1.1_r1.20.rbf"}),
         ConnectionSTREAMImageEntry({LMS_DEV_LIMESDR, 2, 3, "LimeSDR-USB_HW_1.2_r3.0.img", 1, 20, "LimeSDR-USB_HW_1.1_r1.20.rbf"}),
         ConnectionSTREAMImageEntry({LMS_DEV_LIMESDR, 1, 7, "LimeSDR-USB_HW_1.1_r7.0.img", 1, 20, "LimeSDR-USB_HW_1.1_r1.20.rbf"}),
