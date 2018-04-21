9f253d64e404b3b34230da48cd8e454098c141f6
Ricardas Jonaitis <richard.jonaitis@gmail.com>
2018-04-21 15:42:43 +0300

    Update mcu testbench

--- mcu_program/host_src/main.cpp
+++ mcu_program/host_src/main.cpp
@@ -5,6 +5,7 @@
 #include "spi.h"
 #include "mcu.h"
 #include "mcuHexBin.h"
+#include "MCU_BD.h"
 
 #include "LMSBoards.h"
 #include "LMS7002M.h"
@@ -14,6 +15,7 @@
 #include <fstream>
 #include <gnuPlotPipe.h>
 #include <math.h>
+#include "mcu_defines.h"
 using namespace std;
 
 lime::IConnection *serPort = nullptr;
@@ -24,7 +26,7 @@ static bool useMCU =1;
 static bool tx = 1;
 static bool filters = 0;
 static float FBW = 5e6;
-static bool extLoop = true;
+static bool extLoop = false;
 extern float RefClk;
 extern uint8_t extLoopbackPair;
 
@@ -159,6 +161,16 @@ int SetExtLoopback(lime::IConnection* port, uint8_t ch, bool enable)
     return status;
 }
 
+uint8_t UploadMCUParameters(float ref, float bandwidth)
+{
+    uint8_t status;
+    if((status = MCU_SetParameter(MCU_REF_CLK, ref)) != MCU_NO_ERROR)
+        printf("Failed to set Reference Clk\n");
+    if((status = MCU_SetParameter(MCU_BW, bandwidth)) != MCU_NO_ERROR)
+        printf("Failed to set Bandwidth\n");
+    return status;
+}
+
 void DCIQ()
 {
     BoardLoopbackStore loopbackCache(lmsControl.GetConnection());
@@ -175,48 +187,23 @@ void DCIQ()
 
     bool isSetBW = false;
 
+    SetExtLoopback(lmsControl.GetConnection(), 0, extLoop);
     while(freq <= freqEnd)
     {
-        SetExtLoopback(lmsControl.GetConnection(), 0, extLoop);
+
         vfreqs.push_back(freq);
         //status = SetFrequencySX(true, freq + (tx ? 0 : 1e6));
         //status = SetFrequencySX(false, freq + (tx ? -1e6 : 0));
-        if(tx)
-        {
-            //lmsControl.Modify_SPI_Reg_bits(LMS7param(CG_IAMP_TBB), 15);
-            lmsControl.Modify_SPI_Reg_bits(LMS7param(DCCORRI_TXTSP), 1);
-            lmsControl.Modify_SPI_Reg_bits(LMS7param(DCCORRQ_TXTSP), 1);
-            lmsControl.Modify_SPI_Reg_bits(LMS7param(GCORRI_TXTSP), 2047);
-            lmsControl.Modify_SPI_Reg_bits(LMS7param(GCORRQ_TXTSP), 2047);
-            lmsControl.Modify_SPI_Reg_bits(LMS7param(IQCORR_TXTSP), 0);
-            lmsControl.Modify_SPI_Reg_bits(LMS7param(TSGMODE_TXTSP), 0);
-            lmsControl.Modify_SPI_Reg_bits(LMS7param(TSGMODE_TXTSP), 1);
-        }
-        else
-        {
-            lmsControl.Modify_SPI_Reg_bits(LMS7param(DCOFFI_RFE), 0);
-            lmsControl.Modify_SPI_Reg_bits(LMS7param(DCOFFQ_RFE), 0);
-            lmsControl.Modify_SPI_Reg_bits(LMS7param(GCORRI_RXTSP), 2047);
-            lmsControl.Modify_SPI_Reg_bits(LMS7param(GCORRQ_RXTSP), 2047);
-            lmsControl.Modify_SPI_Reg_bits(LMS7param(IQCORR_RXTSP), 0);
-        }
-        lmsControl.DownloadAll();
 
         auto t1 = chrono::high_resolution_clock::now();
         if(useMCU) //using algorithm inside MCU
         {
             if(!isSetBW)
             {
-            status = MCU_SetParameter(MCU_REF_CLK, RefClk);
-            if(status != 0)
-                printf("Failed to set Reference Clk\n");
-            if(filters)
-            {
-                status = MCU_SetParameter(MCU_BW, FBW);
-                if(status != 0)
-                    printf("Failed to set Bandwidth\n");
+                if(UploadMCUParameters(RefClk, FBW) != MCU_NO_ERROR)
+                    return;
                 isSetBW = true;
-            }
+
                 if(extLoop)
                 {
                     uint8_t loopPair = GetExtLoopPair(lmsControl, tx);
@@ -224,7 +211,6 @@ void DCIQ()
                     if(status != 0)
                         printf("Failed to set external loopback pair\n");
                 }
-
             }
 
             if(tx)
@@ -232,10 +218,6 @@ void DCIQ()
             else
                 MCU_RunProcedure(extLoop ? 18 : 2); //initiate Rx calibration
             status = MCU_WaitForStatus(10000); //wait until MCU finishes
-            if(status != 0)
-            {
-                printf("MCU calibration FAILED : 0x%02X\n", status);
-            }
         }
         else //calibrating with PC
         {
@@ -250,86 +232,31 @@ void DCIQ()
         auto t2 = chrono::high_resolution_clock::now();
         long duration = chrono::duration_cast<chrono::milliseconds>(t2 - t1).count();
 
-        lmsControl.DownloadAll();
+        if(status != MCU_NO_ERROR)
+            printf("MCU calibration FAILED : 0x%02X (%s)\n", status, lime::MCU_BD::MCUStatusMessage(status));
+
         int16_t dci, dcq, ph, gi, gq;
-        if(tx)
-        {
-            //dci = lmsControl.Get_SPI_Reg_bits(LMS7param(DCCORRI_TXTSP));
-            //dcq = lmsControl.Get_SPI_Reg_bits(LMS7param(DCCORRQ_TXTSP));
-            dci = ReadDCCorrector(true, 0);
-            dcq = ReadDCCorrector(true, 1);
-            gi = lmsControl.Get_SPI_Reg_bits(LMS7param(GCORRI_TXTSP));
-            gq = lmsControl.Get_SPI_Reg_bits(LMS7param(GCORRQ_TXTSP));
-            ph = lmsControl.Get_SPI_Reg_bits(LMS7param(IQCORR_TXTSP));
+        dci = ReadDCCorrector(tx, 0);
+        dcq = ReadDCCorrector(tx, 1);
+        gi = lmsControl.Get_SPI_Reg_bits(tx ? LMS7param(GCORRI_TXTSP) : LMS7param(GCORRI_RXTSP), true);
+        gq = lmsControl.Get_SPI_Reg_bits(tx ? LMS7param(GCORRQ_TXTSP) : LMS7param(GCORRQ_RXTSP), true);
+        ph = lmsControl.Get_SPI_Reg_bits(tx ? LMS7param(IQCORR_TXTSP) : LMS7param(IQCORR_RXTSP), true);
 
-            ph <<= 4;
-            ph >>= 4;
+        ph <<= 4;
+        ph >>= 4;
+        vdci.push_back(dci);
+        vdcq.push_back(dcq);
+        vgi.push_back(gi);
+        vgq.push_back(gq);
+        vph.push_back(ph);
 
-            vdci.push_back(dci);
-            vdcq.push_back(dcq);
-            vgi.push_back(gi);
-            vgq.push_back(gq);
-            vph.push_back(ph);
-
-            printf("F: %4.0f MHz, DCI:%3i DCQ:%3i GI:%4i GQ:%4i PH:%4i - %s | %li ms\n", freq/1e6, dci, dcq, gi, gq, ph, (status == 0 ? "OK" : "FAIL"), duration);
-        }
-        else
-        {
-            //dci = lmsControl.Get_SPI_Reg_bits(LMS7param(DCOFFI_RFE));
-            //dcq = lmsControl.Get_SPI_Reg_bits(LMS7param(DCOFFQ_RFE));
-            dci = ReadDCCorrector(false, 0);
-            dcq = ReadDCCorrector(false, 1);
-            gi = lmsControl.Get_SPI_Reg_bits(LMS7param(GCORRI_RXTSP));
-            gq = lmsControl.Get_SPI_Reg_bits(LMS7param(GCORRQ_RXTSP));
-            ph = lmsControl.Get_SPI_Reg_bits(LMS7param(IQCORR_RXTSP));
-
-            //int16_t rdci = dci;
-            //int16_t rdcq = dcq;
-            //dci <<= 8;
-            /*if(dci & 0x40)
-                rdci = -1 * abs(dci & 0x3F);
-            if(dcq & 0x40)
-                rdcq = -1 * abs(dcq & 0x3F);*/
-            ph <<= 4;
-            ph >>= 4;
-
-            vdci.push_back(dci);
-            vdcq.push_back(dcq);
-            vgi.push_back(gi);
-            vgq.push_back(gq);
-            vph.push_back(ph);
-
-            printf("F: %4.0f MHz, DCI:%3i DCQ:%3i GI:%4i GQ:%4i PH:%4i - %s | %li ms\n", freq/1e6, dci, dcq, gi, gq, ph, (status == 0 ? "OK" : "FAIL"), duration);
-        }
-        //restore chip state and apply calibration values
-        //RestoreRegisterState();
-        /*if(!useMCU)
-        {
-        lmsControl.RestoreRegisterMap(backup);
-        lmsControl.UploadAll();
-        }*/
-
-        if(tx)
-        {
-            lmsControl.Modify_SPI_Reg_bits(LMS7param(DCCORRI_TXTSP), dci);
-            lmsControl.Modify_SPI_Reg_bits(LMS7param(DCCORRQ_TXTSP), dcq);
-            lmsControl.Modify_SPI_Reg_bits(LMS7param(GCORRI_TXTSP), gi);
-            lmsControl.Modify_SPI_Reg_bits(LMS7param(GCORRQ_TXTSP), gq);
-            lmsControl.Modify_SPI_Reg_bits(LMS7param(IQCORR_TXTSP), ph);
-            lmsControl.Modify_SPI_Reg_bits(LMS7param(TSGMODE_TXTSP), 0);
-            lmsControl.Modify_SPI_Reg_bits(LMS7param(TSGMODE_TXTSP), 1);
-        }
-        else
-        {
-            lmsControl.Modify_SPI_Reg_bits(LMS7param(DCOFFI_RFE), dci);
-            lmsControl.Modify_SPI_Reg_bits(LMS7param(DCOFFQ_RFE), dcq);
-            lmsControl.Modify_SPI_Reg_bits(LMS7param(GCORRI_RXTSP), gi);
-            lmsControl.Modify_SPI_Reg_bits(LMS7param(GCORRQ_RXTSP), gq);
-            lmsControl.Modify_SPI_Reg_bits(LMS7param(IQCORR_RXTSP), ph);
-        }
+        printf("F: %4.0f MHz, DCI:%3i DCQ:%3i GI:%4i GQ:%4i PH:%4i - %s | %li ms\n",
+            freq/1e6, dci, dcq, gi, gq, ph,
+            (status == MCU_NO_ERROR ? "OK" : lime::MCU_BD::MCUStatusMessage(status)),
+            duration);
         freq += freqStep;
-        SetExtLoopback(lmsControl.GetConnection(), 0, false);
     }
+    SetExtLoopback(lmsControl.GetConnection(), 0, false);
     if(tx)
     {
         lmsControl.Modify_SPI_Reg_bits(LMS7param(TSGMODE_TXTSP), 0);
@@ -340,135 +267,43 @@ void DCIQ()
 
 void Filters()
 {
-    lime::LMS7002M_RegistersMap *backup;
     int status;
-    bool isSetBW = false;
-
-    lmsControl.DownloadAll();
-    backup = lmsControl.BackupRegisterMap();
-    (void) backup; // backup set but not used, perhaps for side effects ???
-
     auto t1 = chrono::high_resolution_clock::now();
     if(useMCU) //using algorithm inside MCU
     {
-        if(!isSetBW)
-        {
-        status = MCU_SetParameter(MCU_REF_CLK, RefClk);
-        if(status != 0)
-            printf("Failed to set Reference Clk\n");
-        status = MCU_SetParameter(MCU_BW, FBW);
-        if(status != 0)
-            printf("Failed to set Bandwidth\n");
-        isSetBW = true;
-
-        }
-
-        if(tx)
-            MCU_RunProcedure(6); //initiate Tx calibration
-        else
-            MCU_RunProcedure(5); //initiate Rx calibration
-        status = MCU_WaitForStatus(10000); //wait until MCU finishes
-        if(status != 0)
-        {
-            printf("MCU calibration FAILED : 0x%02X\n", status);
-        }
-
+        if(UploadMCUParameters(RefClk, FBW) != MCU_NO_ERROR)
+            return;
+        MCU_RunProcedure(tx ? 6 : 5); //initiate calibration
+        status = MCU_WaitForStatus(1000); //wait until MCU finishes
     }
     else //calibrating with PC
-    {
-        if(tx)
-            status = TuneTxFilter(FBW);
-        else
-            status = TuneRxFilter(FBW);
-        if(status != 0)
-            printf("Filter tune FAILED");
-    }
+        status = tx ? TuneTxFilter(FBW) : TuneRxFilter(FBW);
+
     auto t2 = chrono::high_resolution_clock::now();
     long duration = chrono::duration_cast<chrono::milliseconds>(t2 - t1).count();
 
-    lmsControl.DownloadAll();
-    int16_t rcal_lpflad,ccal_lpflad, lpfh;
-
-    //Restore settings
-    int cfb_tia_rfe;
-    int ccomp_tia_rfe;
-    int rcomp_tia_rfe;
-    int rcc_ctl_lpfl_rbb;
-    int c_ctl_lpfl_rbb;
-    int c_ctl_pga_rbb;
-    int rcc_ctl_pga_rbb;
-    int rcc_ctl_lpfh_rbb;
-    int c_ctl_lpfh_rbb;
-    int pd_lpfl_rbb;
-    int pd_lpfh_rbb;
-    int input_ctl_pga_rbb;
-
     if(tx)
     {
-        rcal_lpflad = lmsControl.Get_SPI_Reg_bits(LMS7param(RCAL_LPFLAD_TBB));
-        ccal_lpflad = lmsControl.Get_SPI_Reg_bits(LMS7param(CCAL_LPFLAD_TBB));
-        lpfh = lmsControl.Get_SPI_Reg_bits(LMS7param(RCAL_LPFH_TBB));
-
-        printf("Tx filter BW: %.0f MHz, RCAL_LPFLAD: %i, CCAL_LPFLAD: %i, RCAL_LPFH: %i  | %li ms\n",
-                FBW/1e6, rcal_lpflad, ccal_lpflad, lpfh, duration);
+        int16_t rcal_lpflad = lmsControl.Get_SPI_Reg_bits(LMS7param(RCAL_LPFLAD_TBB));
+        int16_t ccal_lpflad = lmsControl.Get_SPI_Reg_bits(LMS7param(CCAL_LPFLAD_TBB));
+        int16_t lpfh = lmsControl.Get_SPI_Reg_bits(LMS7param(RCAL_LPFH_TBB));
+        printf("Tx filter BW: %.0f MHz, RCAL_LPFLAD: %i, CCAL_LPFLAD: %i, RCAL_LPFH: %i | %li ms (%s)\n",
+                FBW/1e6, rcal_lpflad, ccal_lpflad, lpfh, duration,
+                status == MCU_NO_ERROR ? "OK" : lime::MCU_BD::MCUStatusMessage(status));
     }
     else
     {
-        //Restore settings
-        cfb_tia_rfe = lmsControl.Get_SPI_Reg_bits(LMS7param(CFB_TIA_RFE));
-        ccomp_tia_rfe = lmsControl.Get_SPI_Reg_bits(LMS7param(CCOMP_TIA_RFE));
-        rcomp_tia_rfe = lmsControl.Get_SPI_Reg_bits(LMS7param(RCOMP_TIA_RFE));
-        rcc_ctl_lpfl_rbb = lmsControl.Get_SPI_Reg_bits(LMS7param(RCC_CTL_LPFL_RBB));
-        c_ctl_lpfl_rbb = lmsControl.Get_SPI_Reg_bits(LMS7param(C_CTL_LPFL_RBB));
-        c_ctl_pga_rbb = lmsControl.Get_SPI_Reg_bits(LMS7param(C_CTL_PGA_RBB));
-        rcc_ctl_pga_rbb = lmsControl.Get_SPI_Reg_bits(LMS7param(RCC_CTL_PGA_RBB));
-        rcc_ctl_lpfh_rbb = lmsControl.Get_SPI_Reg_bits(LMS7param(RCC_CTL_LPFH_RBB));
-        c_ctl_lpfh_rbb = lmsControl.Get_SPI_Reg_bits(LMS7param(C_CTL_LPFH_RBB));
-        pd_lpfl_rbb = lmsControl.Get_SPI_Reg_bits(LMS7param(PD_LPFL_RBB));
-        pd_lpfh_rbb = lmsControl.Get_SPI_Reg_bits(LMS7param(PD_LPFH_RBB));
-        input_ctl_pga_rbb = lmsControl.Get_SPI_Reg_bits(LMS7param(INPUT_CTL_PGA_RBB));
-
-    }
-    //restore chip state and apply calibration values
-    //RestoreRegisterState();
-   /* if(!useMCU)
-    {
-    lmsControl.RestoreRegisterMap(backup);
-    lmsControl.UploadAll();
-    }*/
-
-    if(tx)
-    {
-        lmsControl.Modify_SPI_Reg_bits(LMS7param(RCAL_LPFLAD_TBB), rcal_lpflad);
-        lmsControl.Modify_SPI_Reg_bits(LMS7param(CCAL_LPFLAD_TBB), ccal_lpflad);
-        lmsControl.Modify_SPI_Reg_bits(LMS7param(RCAL_LPFH_TBB), lpfh);
-    }
-    else
-    {
-        printf("CFB_TIA: %i\n", cfb_tia_rfe);
-        printf("CCOMP: %i\n", ccomp_tia_rfe);
-        printf("RCOMP: %i\n", rcomp_tia_rfe);
-        printf("RCTL_LPFL: %i\n", rcc_ctl_lpfl_rbb);
-        printf("RCTL_LPFH: %i\n", rcc_ctl_lpfh_rbb);
-        printf("C_CTL_LPFL: %i\n", c_ctl_lpfl_rbb);
-        lmsControl.Modify_SPI_Reg_bits(LMS7param(CFB_TIA_RFE), cfb_tia_rfe);
-        lmsControl.Modify_SPI_Reg_bits(LMS7param(CCOMP_TIA_RFE), ccomp_tia_rfe);
-        lmsControl.Modify_SPI_Reg_bits(LMS7param(RCOMP_TIA_RFE), rcomp_tia_rfe);
-        lmsControl.Modify_SPI_Reg_bits(LMS7param(RCC_CTL_LPFL_RBB), rcc_ctl_lpfl_rbb);
-        lmsControl.Modify_SPI_Reg_bits(LMS7param(C_CTL_LPFL_RBB), c_ctl_lpfl_rbb);
-        lmsControl.Modify_SPI_Reg_bits(LMS7param(C_CTL_PGA_RBB), c_ctl_pga_rbb);
-        lmsControl.Modify_SPI_Reg_bits(LMS7param(RCC_CTL_PGA_RBB), rcc_ctl_pga_rbb);
-        lmsControl.Modify_SPI_Reg_bits(LMS7param(RCC_CTL_LPFH_RBB), rcc_ctl_lpfh_rbb);
-        lmsControl.Modify_SPI_Reg_bits(LMS7param(C_CTL_LPFH_RBB), c_ctl_lpfh_rbb);
-        lmsControl.Modify_SPI_Reg_bits(LMS7param(PD_LPFL_RBB), pd_lpfl_rbb);
-        lmsControl.Modify_SPI_Reg_bits(LMS7param(PD_LPFH_RBB), pd_lpfh_rbb);
-        lmsControl.Modify_SPI_Reg_bits(LMS7param(INPUT_CTL_PGA_RBB), input_ctl_pga_rbb);
-        lmsControl.Modify_SPI_Reg_bits(LMS7param(ICT_LPF_IN_RBB), 12);
-        lmsControl.Modify_SPI_Reg_bits(LMS7param(ICT_LPF_OUT_RBB), 12);
-        lmsControl.Modify_SPI_Reg_bits(LMS7param(ICT_PGA_OUT_RBB), 20);
-        lmsControl.Modify_SPI_Reg_bits(LMS7param(ICT_PGA_IN_RBB), 20);
-        lmsControl.Modify_SPI_Reg_bits(LMS7param(R_CTL_LPF_RBB), 16);
-        lmsControl.Modify_SPI_Reg_bits(LMS7param(RFB_TIA_RFE), 16);
+        int cfb_tia_rfe = lmsControl.Get_SPI_Reg_bits(LMS7param(CFB_TIA_RFE), true);
+        int ccomp_tia_rfe = lmsControl.Get_SPI_Reg_bits(LMS7param(CCOMP_TIA_RFE), true);
+        int rcomp_tia_rfe = lmsControl.Get_SPI_Reg_bits(LMS7param(RCOMP_TIA_RFE), true);
+        int rcc_ctl_lpfl_rbb = lmsControl.Get_SPI_Reg_bits(LMS7param(RCC_CTL_LPFL_RBB), true);
+        int c_ctl_lpfl_rbb = lmsControl.Get_SPI_Reg_bits(LMS7param(C_CTL_LPFL_RBB), true);
+        int rcc_ctl_lpfh_rbb = lmsControl.Get_SPI_Reg_bits(LMS7param(RCC_CTL_LPFH_RBB), true);
+        printf("Rx filter BW: %0.f MHz, CFB_TIA: %i, CCOMP: %i, RCOMP: %i, "
+            "RCTL_LPFL: %i, RCTL_LPFH: %i, C_CTL_LPFL: %i | %li ms (%s)\n",
+            FBW/1e6, cfb_tia_rfe, ccomp_tia_rfe, rcomp_tia_rfe, rcc_ctl_lpfl_rbb,
+            rcc_ctl_lpfh_rbb, c_ctl_lpfl_rbb, duration,
+            status == MCU_NO_ERROR ? "OK" : lime::MCU_BD::MCUStatusMessage(status));
     }
 
     if(tx)
@@ -528,15 +363,12 @@ int main(int argc, char** argv)
         lmsControl.Modify_SPI_Reg_bits(LMS7param(TSGMODE_TXTSP), 1);
     }
 
-    int status;
-    //backup chip state
-
     //lmsControl.Modify_SPI_Reg_bits(LMS7param(MAC), 2);
     float crestFactor = 1;
     uint32_t wantedRSSI = 87330 / pow(10.0, (3+crestFactor)/20);
     //RunAGC(wantedRSSI);
 
-    status = 0;
+    int status = 0;
     if(useMCU)
     {
         uint8_t mcuImage[8192*2];
@@ -572,37 +404,5 @@ int main(int argc, char** argv)
     else
         DCIQ();
     lime::ConnectionRegistry::freeConnection(serPort);
-/*
-    GNUPlotPipe plot(true);
-    //plot.write("plot '-' u 1:2 w line title 'DC I', '-' u 1:2 w line title 'DC Q', '-' u 1:2 w line title 'Phase'\n");
-    plot.write("plot '-' u 1:2 w line title 'DC I', '-' u 1:2 w line title 'DC Q', '-' u 1:2 w line title 'Phase', '-' u 1:2 w line title 'GainI', '-' u 1:2 w line title 'GainQ'\n");
-
-    for(size_t i = 0; i < vfreqs.size(); ++i)
-    {
-        plot.writef("%f %i\n", vfreqs[i] / 1e6, vdci[i]);
-    }
-    plot.write("e\n");
-    for(size_t i = 0; i < vfreqs.size(); ++i)
-    {
-        plot.writef("%f %i\n", vfreqs[i] / 1e6, vdcq[i]);
-    }
-    plot.write("e\n");
-    for(size_t i = 0; i < vfreqs.size(); ++i)
-    {
-        plot.writef("%f %i\n", vfreqs[i] / 1e6, vph[i]);
-    }
-    plot.write("e\n");
-    for(size_t i = 0; i < vfreqs.size(); ++i)
-    {
-        plot.writef("%f %i\n", vfreqs[i] / 1e6, vgi[i]);
-    }
-    plot.write("e\n");
-    for(size_t i = 0; i < vfreqs.size(); ++i)
-    {
-        plot.writef("%f %i\n", vfreqs[i] / 1e6, vgq[i]);
-    }
-    plot.write("e\n");
-*/
-
     return 0;
 }
