1d06efda5eb7453e0e548cb9f25adf87b00b4ac4
savajazz <savajazz@gmail.com>
2019-09-12 12:25:44 +0200

    Updated LimeRFE python example

--- src/examples/limeRFE_Python_example.py
+++ src/examples/limeRFE_Python_example.py
@@ -7,43 +7,42 @@ import time
 import limeRFE_H as LH
 
 if os.name == 'nt':
-    libLimeSuite = ctypes.WinDLL ("c:\\path\\to\\LimeSuite.dll")
+    libLimeSuite = ctypes.WinDLL ("C:\\Path\\to\\LimeSuite.dll")
 else:
     libLimeSuite = ctypes.cdll.LoadLibrary('./libLimeSuite.so')
 
-baudrate = 9600
+rfe = c_void_p()
 
 # Open port
 if os.name == 'nt':
-    fd = libLimeSuite.RFE_Open(c_char_p("COM17"), c_int(baudrate));
+    rfe = libLimeSuite.RFE_Open(c_char_p("COM24"), None);
 else:
-    fd = libLimeSuite.RFE_Open(c_char_p("/dev/ttyUSB0"), c_int(baudrate));    
+    rfe = libLimeSuite.RFE_Open(c_char_p("/dev/ttyUSB0"), None);    
 
-if (fd == -1):
-    print(stderr, "Error initializing serial port\n")
+if (rfe == 0):
+    print("Error initializing serial port")
     exit(1)
 
-cinfo = (c_ubyte*4)()
-print "Port opened; fd =", fd
+print "Port opened"
 
 #Configure LimeRFE to use channel HAM 2m channel in receive mode.
 #Transmit output is routed to TX/RX output. Notch is off. Attenuation is 0.
-libLimeSuite.RFE_Configure(c_int(0), c_int(fd), c_int(LH.CID_HAM_0145), c_int(LH.CID_HAM_0145), c_int(LH.PORT_1), c_int(LH.PORT_1), c_int(LH.MODE_RX), c_int(LH.NOTCH_VALUE_OFF), c_int(0))
+result = libLimeSuite.RFE_Configure(rfe, c_int(LH.CID_HAM_0145), c_int(LH.CID_HAM_0145), c_int(LH.PORT_1), c_int(LH.PORT_1), c_int(LH.MODE_RX), c_int(LH.NOTCH_VALUE_OFF), c_int(0))
 if(result != 0):
     print "RFE_Configure Error Code:", result
     print "Check \'LimeRFE error codes' in LimeRFE.h"
-    libLimeSuite.RFE_Close(c_int(fd))
+    libLimeSuite.RFE_Close(rfe)
     exit(1)
 
 time.sleep(1.0)
 
 # Change mode to transmit
-libLimeSuite.RFE_Mode(c_int(0), c_int(fd), c_int(LH.MODE_TX))
+libLimeSuite.RFE_Mode(rfe, c_int(LH.MODE_TX))
 
 time.sleep(1.0)
 
 # Reset LimeRFE
-libLimeSuite.RFE_Reset(c_int(0), c_int(fd))
+libLimeSuite.RFE_Reset(rfe)
 
 # Close port
-libLimeSuite.RFE_Close(c_int(fd))
+libLimeSuite.RFE_Close(rfe)
