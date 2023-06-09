2a6ac985104eed2cc058bf79b1260eedd7c3d4b9
egis <egislev@yandex.com>
2023-06-09 15:28:13 +0300

    added information about SPI and memory devices in LimeDevice

--- src/cli/LimeDevice.cpp
+++ src/cli/LimeDevice.cpp
@@ -56,10 +56,18 @@ int main(int argc, char *argv[])
             std::cout << '\t' << "Firmware version\t: " << d.firmwareVersion << std::endl;
             std::cout << '\t' << "Gateware version\t: " << d.gatewareVersion << std::endl;
             std::cout << '\t' << "Gateware revision\t: " << d.gatewareRevision << std::endl;
-            std::cout << '\t' << "Gateware target board\t: " << d.gatewareTargetBoard << std::endl;
+            std::cout << '\t' << "Gateware target board\t:" << d.gatewareTargetBoard << std::endl;
             std::cout << '\t' << "Hardware version\t: " << d.hardwareVersion << std::endl;
             std::cout << '\t' << "Protocol version\t: " << d.protocolVersion << std::endl;
             std::cout << '\t' << "Serial number\t\t: " << d.serialNumber << std::endl;
+            std::cout << "\t" << "SPI slave devices\t:" << std::endl;
+            for (const auto nameIds : d.spiSlaveIds)
+                std::cout << "\t\t\t\t  " <<  nameIds.first.c_str() << std::endl;
+            std::cout << "\t" << "Memory devices\t\t:" << std::endl;
+            for(const SDRDevice::DataStorage &mem : d.memoryDevices)
+                std::cout << "\t\t\t\t  " << mem.name << std::endl;
+            
+
         }
     }
     std::cout << std::endl;
--- src/cli/LimeFLASH.cpp
+++ src/cli/LimeFLASH.cpp
@@ -7,18 +7,27 @@ SDRDevice::SDRConfig    config;
 
 static int printHelp(void)
 {
-    std::cout << "Usage LimeFLASH [options]" << std::endl;
+    std::cout << "Usage LimeFLASH [options] program.bin" << std::endl;
     std::cout << "    --help\t\t\t This help" << std::endl;
-    std::cout << "    --index <i>\t\t\t Specifies device index (0-... default 0)" << std::endl;
-    std::cout << "    --ram <fname>\t\t RAM programming" << std::endl;
-    std::cout << "    --flash <fname>\t\t FLASH programming" << std::endl;
+    std::cout << "    --index <i>\t\t\t Specifies device index (0-...). Required if multiple devices" << std::endl;
+    std::cout << "    --target <TARGET>\t\t <TARGET> programming. \"-\" to list targets" << std::endl;
 
     return EXIT_SUCCESS;
 }
+
+void listTargets (void)
+{
+    std::cout <<"Possible device targets :" << std::endl;
+    auto d = device->GetDescriptor();
+    
+}
+
 bool CallBack (size_t bsent, size_t btotal, const char* statusMessage)
 {
     return true;
 }
+
+
 /***********************************************************************
  * main entry point
  **********************************************************************/
@@ -27,49 +36,58 @@ int main(int argc, char *argv[])
     static struct option long_options[] = {
         {"help", no_argument, 0, 'h'},
         {"index", required_argument, 0, 'i'},
-        {"ram", required_argument, 0, 'r'},
-        {"flash", required_argument, 0, 'f'},
+        {"target", required_argument, 0, 't'},
         {0, 0, 0,  0}
     };
 
-    int dev_index = 0;
+    int dev_index = -1;
     int long_index = 0;
     int option = 0;
-    char *fram = NULL,*fflash = NULL;
+    int target = -1;
+    char *fn = NULL;
     std::vector<char> data;
     std::ifstream inf;
     
-    while ((option = getopt_long_only(argc, argv, "", long_options, &long_index)) != -1)
+    while ((option = getopt_long_only (argc, argv, "", long_options, &long_index)) != -1)
     {
         switch (option)
         {
         case 'h': 
-            return printHelp();
+            return printHelp ();
         case 'i':
-            if (optarg != NULL) dev_index = std::stod(optarg);
+            if (optarg != NULL) dev_index = std::stod (optarg);
             break;
-        case 'r':
-            if (optarg != NULL) fram = optarg;
-            break;
-        case 'f':
-            if (optarg != NULL) fflash = optarg;
+        case 't':
+            if (optarg != NULL && optarg[0] != '-') 
+               target = std::stod (optarg);
             break;
         }
     }
-    if  (!fram && !fflash) {printHelp (); return EXIT_SUCCESS;}
+    if  (target < 0) {printHelp (); return EXIT_SUCCESS;}
     auto handles = DeviceRegistry::enumerate();
     if (handles.size() == 0) 
     {
         std::cout << "No devices found" << std::endl;
         return -1;
+    } else {
+        if  (handles.size () > 1 && dev_index == -1)
+        {
+            std::cout << "Multiple devices detected and no --index option specified" << std::endl;
+            return -1;
+        } else
+            dev_index = 0;
     }
     device = DeviceRegistry::makeDevice(handles.at(dev_index));
-//    const SDRDevice::Descriptor &desc = device->GetDescriptor();
+    
+    
+    
+    
+    if  (optind == argc) {printHelp (); return -1;}
+//    if  (optind < argc) printf ("%s\n",argv[optind]);
+printf ("%u %u\n",optind,argc);
+    exit;
+//    inf.open (fflash, std::ifstream::in | std::ifstream::binary);
 
-    if  (fram)
-        inf.open (fram, std::ifstream::in | std::ifstream::binary);
-    else
-        inf.open (fflash, std::ifstream::in | std::ifstream::binary);
     if  (!inf)
     {
         std::cout << "Unable to open file." << std::endl;
@@ -82,13 +100,13 @@ int main(int argc, char *argv[])
     data.resize (cnt);
     inf.read (data.data (),cnt);
     inf.close ();
-    if (device->UploadMemory (fram?0:1, data.data(), data.size(), CallBack))
+  /*  if (device->UploadMemory (fram?0:1, data.data(), data.size(), CallBack))
     {
         std::cout << "Error programming device." << std::endl;
         return -1;
     }
     std::cout << "Ok." << std::endl;
-
+*/
     return EXIT_SUCCESS;
 }
 
