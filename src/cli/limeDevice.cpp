c83bd7bd297b6ead13d7f33c16f4a9c17601af21
Ricardas Jonaitis <richard.jonaitis@gmail.com>
2023-07-31 22:23:50 +0300

    Cleanup limeDevice

--- src/cli/limeDevice.cpp
+++ src/cli/limeDevice.cpp
@@ -1,21 +1,35 @@
 #include "cli/common.h"
 
+using namespace std;
 using namespace lime;
-
-SDRDevice               *device = nullptr;
-SDRDevice::SDRConfig    config;
 
 static int printHelp(void)
 {
-    std::cout << "Usage LimeDevice [options]" << std::endl;
-    std::cout << "    --help\t This help" << std::endl;
-    std::cout << "    --full\t Force print detailed device(s) info" << std::endl;
+    cout << "limeDevice [options]" << endl;
+    cout << "    -h, --help\t This help" << endl;
+    cout << "    -f, --full\t Force print detailed device(s) info" << endl;
     return EXIT_SUCCESS;
 }
 
-/***********************************************************************
- * main entry point
- **********************************************************************/
+void PrintDeviceDetails(SDRDevice* device)
+{
+    auto d = device->GetDescriptor();
+    cout << '\t' << "Expansion name\t\t: " << d.expansionName << endl;
+    cout << '\t' << "Firmware version\t: " << d.firmwareVersion << endl;
+    cout << '\t' << "Gateware version\t: " << d.gatewareVersion << endl;
+    cout << '\t' << "Gateware revision\t: " << d.gatewareRevision << endl;
+    cout << '\t' << "Gateware target board\t: " << d.gatewareTargetBoard << endl;
+    cout << '\t' << "Hardware version\t: " << d.hardwareVersion << endl;
+    cout << '\t' << "Protocol version\t: " << d.protocolVersion << endl;
+    cout << '\t' << "Serial number\t\t: " << d.serialNumber << endl;
+    cout << "\t" << "SPI slave devices\t:" << endl;
+    for (const auto &nameIds : d.spiSlaveIds)
+        cout << "\t\t\t\t  " <<  nameIds.first.c_str() << endl;
+    cout << "\t" << "Memory devices\t\t:" << endl;
+    for (const SDRDevice::DataStorage &mem : d.memoryDevices)
+        cout << "\t\t\t\t  " << mem.name << endl;
+}
+
 int main(int argc, char *argv[])
 {
     static struct option long_options[] = {
@@ -44,30 +58,22 @@ int main(int argc, char *argv[])
         printf("No devices found\n");
         return -1;
     }
-    std::cout << "Found " << handles.size() << " device(s) :" << std::endl;
+    cout << "Found " << handles.size() << " device(s) :" << endl;
     for (uint32_t i = 0; i < handles.size(); i++)
     {
-        std::cout << i << ": " << handles[i].serialize() << std::endl;
-        if  (full)
+        cout << i << ": " << handles[i].serialize() << endl;
+        if (full)
         {
-            device = DeviceRegistry::makeDevice(handles.at(i));
-            auto d = device->GetDescriptor();
-            std::cout << '\t' << "Expansion name\t\t: " << d.expansionName << std::endl;
-            std::cout << '\t' << "Firmware version\t: " << d.firmwareVersion << std::endl;
-            std::cout << '\t' << "Gateware version\t: " << d.gatewareVersion << std::endl;
-            std::cout << '\t' << "Gateware revision\t: " << d.gatewareRevision << std::endl;
-            std::cout << '\t' << "Gateware target board\t:" << d.gatewareTargetBoard << std::endl;
-            std::cout << '\t' << "Hardware version\t: " << d.hardwareVersion << std::endl;
-            std::cout << '\t' << "Protocol version\t: " << d.protocolVersion << std::endl;
-            std::cout << '\t' << "Serial number\t\t: " << d.serialNumber << std::endl;
-            std::cout << "\t" << "SPI slave devices\t:" << std::endl;
-            for (const auto nameIds : d.spiSlaveIds)
-                std::cout << "\t\t\t\t  " <<  nameIds.first.c_str() << std::endl;
-            std::cout << "\t" << "Memory devices\t\t:" << std::endl;
-            for(const SDRDevice::DataStorage &mem : d.memoryDevices)
-                std::cout << "\t\t\t\t  " << mem.name << std::endl;
+            SDRDevice* device = DeviceRegistry::makeDevice(handles.at(i));
+            if (!device)
+            {
+                cout << "\tFailed to connect" << endl;
+                continue;
+            }
+            PrintDeviceDetails(device);
+            DeviceRegistry::freeDevice(device);
         }
     }
-    std::cout << std::endl;
+    cout << endl;
     return EXIT_SUCCESS;
 }
