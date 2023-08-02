44d78e685e952d27f84e50a3904aad0bc8cebc0c
Ricardas Jonaitis <richard.jonaitis@gmail.com>
2023-08-02 16:45:38 +0300

    Add missing cli/common.cpp

---
+++ src/cli/common.cpp
@@ -0,0 +1,17 @@
+#include "common.h"
+
+using namespace lime;
+
+SDRDevice* ConnectUsingNameHint(const char* hintArguments)
+{
+    DeviceHandle handle(hintArguments);
+    auto handles = DeviceRegistry::enumerate(handle);
+    if (handles.size() > 1)
+    {
+        std::cerr << "Ambiguous device handle (" << handle.Serialize() << "), matches:\n";
+        for (const auto& h : handles)
+            std::cerr << "\t" << h.Serialize() << std::endl;
+        return nullptr;
+    }
+    return DeviceRegistry::makeDevice(handle);
+}
