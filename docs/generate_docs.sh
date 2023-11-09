6aa9c061e9cf4ac05cfd8777bb17338f7994cdcd
Dominykas <dominykas.petkevicius@matom.ai>
2023-11-09 15:10:51 +0200

    Improve doc generation script

--- docs/generate_docs.sh
+++ docs/generate_docs.sh
@@ -1,14 +1,30 @@
 #!/bin/bash
 ## Must be run from within the venv.
 
+# Usage:
+# ./generate_docs.sh - to just regenerate the main docs
+# ./generate_docs.sh clean - to clean all the build files
+# ./generate_docs.sh rebuild - to rebuild the doxygen definitions
+# ./generate_docs.sh clean rebuild - to clean the build files and rebuild the definitions
+
+set -e
+
 if [[ $1 == "clean" ]]; then
+    echo "Deleting _build folder"
     rm -rf _build
-    set "rebuild"
+
+    echo "Deleting apidoc folder"
+    rm -rf apidoc
+
+    shift
+    if [[ $1 == "" ]]; then
+        exit 0
+    fi
 fi
 
 if [[ $1 == "rebuild" ]]; then
+    cmake -S .. -B ../build
     make --no-print-directory -C ../build doc
-    rm -rf apidoc
     breathe-apidoc --generate class --members --force --output-dir apidoc ../build/xml/
 fi
 
