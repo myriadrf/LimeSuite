56e36372039689ea99390f5244fc1ecc0586f64f
Dominykas <dominykas.petkevicius@matom.ai>
2023-11-17 14:00:30 +0200

    Clean up "magic"

--- src/tests/comms/USB/USB_CSR_Pipe_SDRTest.cpp
+++ src/tests/comms/USB/USB_CSR_Pipe_SDRTest.cpp
@@ -19,7 +19,7 @@ TEST(USB_CSR_Pipe_SDR, WriteBulkTransfer)
     pkt.cmd = LMS64CProtocol::CMD_GPIO_RD;
 
     EXPECT_CALL(mockConnection, BulkTransfer(_, _, _, _)).Times(1);
-    pipe.Write((uint8_t*)&pkt, sizeof(pkt), 100);
+    pipe.Write(reinterpret_cast<uint8_t*>(&pkt), sizeof(pkt), 100);
 }
 
 TEST(USB_CSR_Pipe_SDR, WriteControlTransfer)
@@ -31,7 +31,7 @@ TEST(USB_CSR_Pipe_SDR, WriteControlTransfer)
     pkt.cmd = LMS64CProtocol::CMD_GET_INFO;
 
     EXPECT_CALL(mockConnection, ControlTransfer(_, _, _, _, _, _, _)).Times(1);
-    pipe.Write((uint8_t*)&pkt, sizeof(pkt), 100);
+    pipe.Write(reinterpret_cast<uint8_t*>(&pkt), sizeof(pkt), 100);
 }
 
 TEST(USB_CSR_Pipe_SDR, ReadBulkTransfer)
@@ -43,7 +43,7 @@ TEST(USB_CSR_Pipe_SDR, ReadBulkTransfer)
     pkt.cmd = LMS64CProtocol::CMD_GPIO_RD;
 
     EXPECT_CALL(mockConnection, BulkTransfer(_, _, _, _)).Times(1);
-    pipe.Read((uint8_t*)&pkt, sizeof(pkt), 100);
+    pipe.Read(reinterpret_cast<uint8_t*>(&pkt), sizeof(pkt), 100);
 }
 
 TEST(USB_CSR_Pipe_SDR, ReadControlTransfer)
@@ -55,5 +55,5 @@ TEST(USB_CSR_Pipe_SDR, ReadControlTransfer)
     pkt.cmd = LMS64CProtocol::CMD_GET_INFO;
 
     EXPECT_CALL(mockConnection, ControlTransfer(_, _, _, _, _, _, _)).Times(1);
-    pipe.Read((uint8_t*)&pkt, sizeof(pkt), 100);
+    pipe.Read(reinterpret_cast<uint8_t*>(&pkt), sizeof(pkt), 100);
 }
