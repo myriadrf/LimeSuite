6bf9661e3f086e6761104d712a2b2fd1c960ece6
Dominykas <dominykas.petkevicius@matom.ai>
2023-11-17 14:00:19 +0200

    SPI function cleanup

--- src/boards/LimeSDR/LimeSDR.cpp
+++ src/boards/LimeSDR/LimeSDR.cpp
@@ -473,15 +473,17 @@ void LimeSDR::SPI(uint32_t chipSelect, const uint32_t* MOSI, uint32_t* MISO, uin
     assert(mSerialPort);
     assert(MOSI);
     LMS64CPacket pkt;
-    pkt.status = LMS64CProtocol::STATUS_UNDEFINED;
-    pkt.blockCount = 0;
-    pkt.periphID = chipSelect;
 
     size_t srcIndex = 0;
     size_t destIndex = 0;
-    const int maxBlocks = 14;
+    constexpr int maxBlocks = LMS64CPacket::payloadSize / (sizeof(uint32_t) / sizeof(uint8_t)); // = 14
+
     while (srcIndex < count)
     {
+        pkt.status = LMS64CProtocol::STATUS_UNDEFINED;
+        pkt.blockCount = 0;
+        pkt.periphID = chipSelect;
+
         // fill packet with same direction operations
         const bool willDoWrite = MOSI[srcIndex] & (1 << 31);
         for (int i = 0; i < maxBlocks && srcIndex < count; ++i)
@@ -551,9 +553,9 @@ void LimeSDR::SPI(uint32_t chipSelect, const uint32_t* MOSI, uint32_t* MISO, uin
             }
         }
         else
+        {
             throw std::runtime_error("SPI failed");
-        pkt.blockCount = 0;
-        pkt.status = LMS64CProtocol::STATUS_UNDEFINED;
+        }
     }
 }
 
