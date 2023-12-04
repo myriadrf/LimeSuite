2826cae218247b58f12366ef789298a0356eaf8d
Dominykas <dominykas.petkevicius@matom.ai>
2023-12-04 12:41:04 +0200

    Add TODOs to Gain functions

--- src/API/LMS_APIWrapper.cpp
+++ src/API/LMS_APIWrapper.cpp
@@ -646,6 +646,7 @@ API_EXPORT int CALL_CONV LMS_GetLPFBWRange(lms_device_t* device, bool dir_tx, lm
     return 0;
 }
 
+// TODO: Implement properly once the Gain API is completed
 API_EXPORT int CALL_CONV LMS_SetNormalizedGain(lms_device_t* device, bool dir_tx, size_t chan, float_type gain)
 {
     LMS_APIDevice* apiDevice = CheckDevice(device, chan);
@@ -690,6 +691,7 @@ API_EXPORT int CALL_CONV LMS_SetNormalizedGain(lms_device_t* device, bool dir_tx
     return 0;
 }
 
+// TODO: Implement properly once the Gain API is completed
 API_EXPORT int CALL_CONV LMS_SetGaindB(lms_device_t* device, bool dir_tx, size_t chan, unsigned gain)
 {
     LMS_APIDevice* apiDevice = CheckDevice(device, chan);
@@ -722,6 +724,7 @@ API_EXPORT int CALL_CONV LMS_SetGaindB(lms_device_t* device, bool dir_tx, size_t
     return 0;
 }
 
+// TODO: Implement properly once the Gain API is completed
 API_EXPORT int CALL_CONV LMS_GetNormalizedGain(lms_device_t* device, bool dir_tx, size_t chan, float_type* gain)
 {
     LMS_APIDevice* apiDevice = CheckDevice(device, chan);
@@ -736,6 +739,7 @@ API_EXPORT int CALL_CONV LMS_GetNormalizedGain(lms_device_t* device, bool dir_tx
     return LMS_SUCCESS;
 }
 
+// TODO: Implement properly once the Gain API is completed
 API_EXPORT int CALL_CONV LMS_GetGaindB(lms_device_t* device, bool dir_tx, size_t chan, unsigned* gain)
 {
     LMS_APIDevice* apiDevice = CheckDevice(device, chan);
