b1b3720da290f48af23184f87ec618de9a1549ee
Dominykas <dominykas.petkevicius@matom.ai>
2023-12-13 15:09:14 +0200

    Revert limeRFE files

--- src/limeRFE/limeRFE.h
+++ src/limeRFE/limeRFE.h
@@ -139,7 +139,7 @@ extern "C" {
 #   else
 #       define API_EXPORT __declspec(dllexport)
 #   endif
-#elif __DOXYGEN__
+#elif defined _DOXYGEN_ONLY_
 	/** Marks an API routine to be made visible to the dynamic loader.
 	*  This is OS and/or compiler-specific. */
 #   define API_EXPORT
