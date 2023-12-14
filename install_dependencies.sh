20339cbd887e7519978987b77c0023516fca098a
Dominykas <dominykas.petkevicius@matom.ai>
2023-12-14 15:50:10 +0200

    Add more robust distribution type checking

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -7,15 +7,37 @@ if [ "$EUID" -ne 0 ]; then
   exit 1
 fi
 
+ID=""
+ID_LIKE=""
+TYPE=""
 . /etc/os-release
+
+if [[ $ID == "ubuntu" || $ID == "debian" ]]; then
+  TYPE=$ID
+fi
+
+if [[ $TYPE == "" ]]; then
+  LIKE=${ID_LIKE%% *}
+
+  if [[ $LIKE == "ubuntu" ]]; then
+    TYPE="ubuntu"
+  elif [[ $LIKE == "debian" ]]; then
+    TYPE="debian"
+  fi
+fi
+
+if [[ $TYPE == "" ]]; then
+  echo "Current distribution is very likely currently unsupported, aborting."
+  exit 1
+fi
 
 libwx="libwxgtk3.2-dev"
 
-if [[ ($ID == "ubuntu" && ${VERSION_ID:0:2} -le 22) || ($ID == "debian" && $VERSION_ID -le 11) ]]; then
+if [[ ($TYPE == "ubuntu" && ${VERSION_ID:0:2} -le 22) || ($TYPE == "debian" && $VERSION_ID -le 11) ]]; then
   libwx="libwxbase3.0-dev libwxgtk3.0-gtk3-dev"
 fi
 
-apt-get install -y \
+apt-get install \
   libusb-1.0-0-dev \
   build-essential \
   $libwx \
