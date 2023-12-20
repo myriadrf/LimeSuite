288567d250083b88b90f91097a1ac68a894e9755
Dominykas <dominykas.petkevicius@matom.ai>
2023-12-20 12:36:59 +0200

    Document DeltaVariable

--- src/include/limesuite/commonTypes.h
+++ src/include/limesuite/commonTypes.h
@@ -21,13 +21,39 @@ struct Range {
 template<class T> class DeltaVariable
 {
   public:
+    /**
+    @brief Construct a new DeltaVariable object.
+    @param init The initial value of the variable.
+   */
     DeltaVariable(T init)
         : mValue(init)
         , mLastValue(0){};
+
+    /**
+      @brief Sets the value of the delta variable.
+      @param val The value to set the delta variable to.
+     */
     void set(T val) { mValue = val; }
+
+    /**
+      @brief Adds the given value to the delta variable
+      @param val The value to add to the delta variable.
+     */
     void add(T val) { mValue += val; }
-    T delta() { return mValue - mLastValue; } // value change since last reset
+
+    /**
+      @brief Gets the difference between the value at last reset and current value.
+      @return T The delta between the last time the delta variable was reset and the current value.
+     */
+    T delta() const { return mValue - mLastValue; }
+
+    /**
+      @brief Gets the current value of the delta variable.
+      @return T The current stored value of the delta variable.
+     */
     T value() const { return mValue; }
+
+    /** @brief Resets the delta variable to start measuring from its current value. */
     void checkpoint() { mLastValue = mValue; }
 
   private:
