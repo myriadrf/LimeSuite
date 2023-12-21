84e9477d39650040f310532b443d5fac8e691734
Dominykas <dominykas.petkevicius@matom.ai>
2023-12-21 11:40:07 +0200

    Add SlaveSelectShim documentation

--- src/boards/LimeSDR_X3/SlaveSelectShim.h
+++ src/boards/LimeSDR_X3/SlaveSelectShim.h
@@ -10,9 +10,19 @@ namespace lime {
 class SlaveSelectShim : public ISPI
 {
   public:
+  /**
+    @brief Construct a new Slave Select Shim object
+    @param comms The communications interface to use.
+    @param slaveId The ID of the slave for this shim.
+   */
     SlaveSelectShim(std::shared_ptr<IComms> comms, uint32_t slaveId);
     virtual int SPI(const uint32_t* MOSI, uint32_t* MISO, uint32_t count) override;
     virtual int SPI(uint32_t spiBusAddress, const uint32_t* MOSI, uint32_t* MISO, uint32_t count) override;
+
+    /**
+      @brief Send the reset device command to the device under this shim.
+      @return int The success status of the operation (0 on success),
+     */
     virtual int ResetDevice();
 
   private:
