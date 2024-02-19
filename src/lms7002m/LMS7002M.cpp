16cc771342fc5ce7b6998d6485ce0d878a725371
Dominykas <dominykas.petkevicius@matom.ai>
2024-02-19 13:56:09 +0200

    Fix warnings in LMS7002M code

--- src/lms7002m/LMS7002M.cpp
+++ src/lms7002m/LMS7002M.cpp
@@ -19,17 +19,18 @@
 #include <unordered_set>
 #include <thread>
 
-#include "cpp-feather-ini-parser/INI.h"
-#include "limesuite/IComms.h"
-#include "limesuite/commonTypes.h"
 #ifdef __GNUC__
     #pragma GCC diagnostic push
     #pragma GCC diagnostic ignored "-Wold-style-cast"
 #endif
+#include "cpp-feather-ini-parser/INI.h"
 #include "lms_gfir.h"
 #ifdef __GNUC__
     #pragma GCC diagnostic pop
 #endif
+
+#include "limesuite/commonTypes.h"
+#include "limesuite/IComms.h"
 #include "LMS7002M_RegistersMap.h"
 #include "Logger.h"
 #include "mcu_programs.h"
@@ -1765,9 +1766,9 @@ OpStatus LMS7002M::Modify_SPI_Reg_mask(const uint16_t* addr, const uint16_t* mas
         data.push_back(reg_data);
         ++start;
     }
+    status = SPI_write_batch(&addresses[0], &data[0], addresses.size());
     if (status != OpStatus::SUCCESS)
         return status;
-    status = SPI_write_batch(&addresses[0], &data[0], addresses.size());
     return status;
 }
 
