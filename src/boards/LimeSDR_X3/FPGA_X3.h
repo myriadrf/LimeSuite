30307f62be30ee5e80596cc5e6091cd1a0f925bb
Ricardas Jonaitis <richard.jonaitis@gmail.com>
2024-02-20 12:46:19 +0200

    Fix FPGA interface clocks configuring

--- src/boards/LimeSDR_X3/FPGA_X3.h
+++ src/boards/LimeSDR_X3/FPGA_X3.h
@@ -13,9 +13,11 @@ class FPGA_X3 : public FPGA
   public:
     FPGA_X3(std::shared_ptr<ISPI> fpgaSPI, std::shared_ptr<ISPI> lms7002mSPI);
     virtual ~FPGA_X3(){};
-    OpStatus SetInterfaceFreq(double f_Tx_Hz, double f_Rx_Hz, double txPhase, double rxPhase, int ch = 0) override;
-    OpStatus SetInterfaceFreq(double f_Tx_Hz, double f_Rx_Hz, int ch = 0) override;
-    OpStatus SetPllFrequency(const uint8_t pllIndex, const double inputFreq, FPGA_PLL_clock* clocks, const uint8_t clockCount);
+    OpStatus SetInterfaceFreq(double f_Tx_Hz, double f_Rx_Hz, double txPhase, double rxPhase, int chipIndex = 0) override;
+    OpStatus SetInterfaceFreq(double f_Tx_Hz, double f_Rx_Hz, int chipIndex = 0) override;
+
+  protected:
+    virtual OpStatus SetPllFrequency(const uint8_t pllIndex, const double inputFreq, std::vector<FPGA_PLL_clock>& clocks) override;
 };
 
 } // namespace lime
