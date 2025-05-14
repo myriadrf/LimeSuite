a0acda7c29095fe269d85b34faa2fbdf5b0120ef
Agustin Catellani <agustin.catellani@gmail.com>
2025-05-14 10:04:58 -0300

    feat: improve channel selection logic

--- SoapyLMS7/Streaming.cpp
+++ SoapyLMS7/Streaming.cpp
@@ -155,24 +155,22 @@ SoapySDR::Stream *SoapyLMS7::setupStream(
     config.bufferLength = 0; //auto
 
     std::vector<size_t> chans = {};
-    if(_deviceArgs.count("subdev")) {
+
+    /* Try to use the channels passed in the `subdev` device argument */
+    if (_deviceArgs.count("subdev")) {
         const std::string subdev_str = _deviceArgs.at("subdev");
 
         size_t num;
         std::stringstream ss(subdev_str);
-        while(ss >> num) {
+        while (ss >> num) {
             chans.push_back(num);
             SoapySDR::logf(SOAPY_SDR_INFO, "Using channel: %zu", num);
         }
+    } else {
+        /* If `subdev` was not present, try to use channels passed as an
+         * argument to this function, or default to using channel 0. */
+        chans = channels.empty() ? std::vector<size_t>{0} : channels;
     }
-
-    if (chans.empty()) {
-      SoapySDR::logf(SOAPY_SDR_INFO, "Defaulting to channel 0");
-      chans.push_back(0);
-    }
-
-    //default to channel 0, if none were specified
-    // const std::vector<size_t> &channelIDs = channels.empty() ? std::vector<size_t>{0} : channels;
 
     const std::vector<size_t> &channelIDs = chans;
     for(size_t i=0; i<channelIDs.size(); ++i)
